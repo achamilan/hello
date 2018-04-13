c72e229d2b003848900424befad9006e60f6ddd7
chenlijia1111 <clj362321@>
2018-04-13 17:42:56 +0800

    1.0

---
+++ src/main/java/com/hello/controller/Mi.java
@@ -0,0 +1,12 @@
+package com.hello.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Mi {
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello";
+    }
+}
